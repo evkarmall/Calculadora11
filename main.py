8198f2ecf86e421d3764e3160bd6abfdfc23047c
Jin Mori <pauloh.des.ti@gmail.com>
2023-12-13 15:49:37 -0300

    concluido

--- main.py
+++ main.py
@@ -9,30 +9,56 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.show()
-        self.ui.zerobotao.clicked.connect()
-        self.ui.um_botao.clicked.connect()
-        self.ui.dois_botao.clicked.connect()
-        self.ui.tres_botao.clicked.connect()
-        self.ui.quatro_botao.clicked.connect()
-        self.ui.cinco_botao.clicked.connect()
-        self.ui.seis_botao.clicked.connect()
-        self.ui.sete_botao.clicked.connect()
-        self.ui.oito_botao.clicked.connect()
-        self.ui.nove_botao.clicked.connect()
-        self.ui.virgulabotao.clicked.connect()
-        self.ui.multiplicacaobotao.clicked.connect()
-        self.ui.subtracaobotao.connect()
-        self.ui.adicao_botao.clicked.connect()
-        self.ui.divisaoBotao.clicked.connect()
-        self.ui.porcentagembotao.clicked.connect()
-        self.ui.clearBottom.clicked.connect()
-        self.ui.clearAllBottom.clicked.connect()
-        self.ui.igualbotao.clicked.connect()
-        
+        self.ui.zerobotao.clicked.connect(lambda: self.pressionado("0"))
+        self.ui.um_botao.clicked.connect(lambda: self.pressionado("1"))
+        self.ui.dois_botao.clicked.connect(lambda: self.pressionado("2"))
+        self.ui.tres_botao.clicked.connect(lambda: self.pressionado("3"))
+        self.ui.quatro_botao.clicked.connect(lambda: self.pressionado("4"))
+        self.ui.cinco_botao.clicked.connect(lambda: self.pressionado("5"))
+        self.ui.seis_botao.clicked.connect(lambda: self.pressionado("6"))
+        self.ui.sete_botao.clicked.connect(lambda: self.pressionado("7"))
+        self.ui.oito_botao.clicked.connect(lambda: self.pressionado("8"))
+        self.ui.nove_botao.clicked.connect(lambda: self.pressionado("9"))
+        self.ui.virgulabotao.clicked.connect(lambda: self.pressionado(","))
+        self.ui.multiplicacaobotao.clicked.connect(lambda: self.pressionado("*"))
+        self.ui.subtracaobotao.clicked.connect(lambda: self.pressionado("-"))
+        self.ui.adicao_botao.clicked.connect(lambda: self.pressionado("+"))
+        self.ui.divisaoBotao.clicked.connect(lambda: self.pressionado("/"))
+        self.ui.porcentagembotao.clicked.connect(lambda: self.pressionado("%"))
+        self.ui.clearBottom.clicked.connect(lambda: self.backspace())
+        self.ui.clearAllBottom.clicked.connect(lambda: self.clearAllOutput())
+        self.ui.igualbotao.clicked.connect(lambda: self.resultado())
 
-        
+    def pressionado(self, tecla):
+        saida = self.ui.outputLabel.text()
+        if tecla == "," and "," in saida:
+            return
+        if saida == "0":
+            saida = " "
+        if tecla in ["+", "-", "*", "/", ","] and saida[-1] in ["+", "-", "*", "/", ","]:
+            saida = saida[:-1]
+        saida += tecla
+        self.ui.outputLabel.setText(saida)
 
+    def resultado(self):
+        saida = self.ui.outputLabel.text()
+        try:
+            resposta = eval(saida)
+            self.ui.outputLabel.setText(f"{resposta}")
+            if saida == "%":
+                saida /= 100
+        except:
+            self.ui.outputLabel.setText("ERRO")
+            
 
+    def backspace(self):
+        saida = self.ui.outputLabel.text()
+        self.ui.outputLabel.setText(saida[:-1])
+        if not len(saida[:-1]):
+            self.clearAllOutput()
+
+    def clearAllOutput(self):
+        self.ui.outputLabel.setText(" ")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
